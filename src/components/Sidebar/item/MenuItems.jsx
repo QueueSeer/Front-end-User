07e75ec9fd5c78af9c6ddb55a82b5aaef83281db
SURANGKANANG <ploylol3966@gmail.com>
2025-03-28 08:22:01 +0700

    ีupdate

--- src/components/Sidebar/item/MenuItems.jsx
+++ src/components/Sidebar/item/MenuItems.jsx
@@ -4,7 +4,7 @@ const MenuItems = [
   { icon: Images.UserIcon, text: "โปรไฟล์", href: "/profileme" },
   { icon: Images.calendarIcon, text: "จองคิว", href: "/queuehistory" },
   { icon: Images.OutlineIcon, text: "การชำระเงิน", href: ["/paymentHistoryPage"] }, 
-  { icon: Images.Users_GroupIcon, text: "ผู้ติดตาม", href: "/follower" },
+  { icon: Images.Users_GroupIcon, text: "กำลังติดตาม", href: "/follower" },
 ];
 
 export default MenuItems;
--- src/components/navbar/NavbarProfile/UserInfo.jsx
+++ src/components/navbar/NavbarProfile/UserInfo.jsx
@@ -1,20 +1,33 @@
-// src/components/UserInfo.jsx
 import React from "react";
 
-const UserInfo = ({ name, username, border }) => {
+const UserInfo = ({ name, username, border, image }) => {
   return (
     <div
-      className={`px-4 py-2 ${
+      className={`flex flex-col items-center py-5 px-4 ${
         border ? "border-b border-gray-200 dark:border-gray-600" : ""
       }`}
     >
-      <span className="block text-gray-900 dark:text-white font-sans font-semibold text-[20px] mb-1.5">
-        {/* เพิ่ม mb-1 (margin-bottom) เพื่อสร้างระยะห่าง */}
+      {/* รูปโปรไฟล์ */}
+      <img
+        className="w-16 h-16 rounded-full mb-2"
+        src={image || "https://via.placeholder.com/150"}
+        alt={name}
+        onError={(e) => {
+          e.target.src = "https://via.placeholder.com/150";
+        }}
+      />
+      
+      {/* ชื่อผู้ใช้ */}
+      <span className="block text-gray-900 dark:text-white font-sans font-semibold text-xl mb-1.5 text-[#420F75]">
         {name}
       </span>
-      {/* <span className="block text-gray-500 truncate dark:text-gray-400 font-sans text-sm">
-        {username}
-      </span> */}
+      
+      {/* ชื่อผู้ใช้ในระบบ */}
+      {username && (
+        <span className="block text-gray-500 truncate dark:text-gray-400 font-sans text-sm">
+          @{username}
+        </span>
+      )}
     </div>
   );
 };
